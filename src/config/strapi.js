f8aa3d7566b5ed46fdf32fdc96d806acc92d903d
bluesarttra <blue@degitobangkok.com>
2025-09-24 11:40:43 +0700

    updatee

--- src/config/strapi.js
+++ src/config/strapi.js
@@ -20,7 +20,6 @@ export async function isCMSAvailable() {
     
     return response.ok;
   } catch (error) {
-    console.warn('CMS not available, using fallback mode:', error.message);
     return false;
   }
 }
